6beb21994215d994e03e6189e79d55480f345f7b
Peddi Shiva <140646940+peddishiva@users.noreply.github.com>
2026-01-29 20:18:29 +0530

    updated pass

--- src/reporting.py
+++ src/reporting.py
@@ -9,7 +9,7 @@ class ReportGenerator:
     def __init__(self, output_dir="customer_auditor/data/reports"):
         self.output_dir = output_dir
         self.sender_email = "peddishiva63@gmail.com"
-        self.sender_pass = "myjn fwkq lyjo gopn"
+        self.sender_pass = "tgxf rqaa ynuy rfqc"
 
     def generate_pdf(self, audit_data, filename="audit_report.pdf"):
         pdf = FPDF()
